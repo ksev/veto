0b0294992b4311d2b05c0cd17a098c98887d7791
Kim Sevandersson <kim@kotee.co>
2024-03-28 22:30:07 +0100

    Enable 1password cli and ssh-agent

--- flake.nix
+++ flake.nix
@@ -3,11 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    sops-nix = { 
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -18,12 +13,14 @@
   outputs = {
     nixpkgs,
     home-manager,
-    sops-nix,
     ...
   }: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
     homeConfigurations."kim" = home-manager.lib.homeManagerConfiguration {
       inherit pkgs;
 
@@ -37,16 +34,17 @@
       ];
     };
 
-    devShell."${system}" = with pkgs; mkShell {
-      buildInputs = [
-        sops
-        git      
-      ];
+    devShell."${system}" = with pkgs;
+      mkShell {
+        buildInputs = [
+          sops
+          git
+        ];
 
-      shellHook = ''
-        ROOT_PATH=$(git rev-parse --show-toplevel)
-        export SOPS_AGE_KEY_FILE="$ROOT_PATH/sops-keys.txt"
-      '';
-    };
+        shellHook = ''
+          ROOT_PATH=$(git rev-parse --show-toplevel)
+          export SOPS_AGE_KEY_FILE="$ROOT_PATH/sops-keys.txt"
+        '';
+      };
   };
 }
--- home/default.nix
+++ home/default.nix
@@ -1,10 +1,29 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  onePassPath = "~/.1password/agent.sock";
+in {
   home.username = "kim";
   home.stateVersion = "23.11"; # Please read the comment before changing.
   home.homeDirectory = lib.mkDefault "/home/kim";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    _1password
+  ];
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host *
+        IdentitiesOnly=yes
+        IdentityAgent ${onePassPath}
+    '';
+  };
 
   imports = [
     ./shell
