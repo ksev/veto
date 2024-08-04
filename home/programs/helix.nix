3f06e025578402102039777e1ea74c02b8f5d568
Kim Sevandersson <kim@kotee.co>
2024-08-04 12:28:57 +0200

    Define lsp and formatter for nix in helix

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -3,21 +3,36 @@
     enable = true;
     defaultEditor = true;
 
-    /*
     languages = {
+      language-server.nixd = {
+        command = "nixd";
+        args = ["--log" "error"];
+      };
+
+      /*
       language-server.roslyn-ls = {
         command = "Microsoft.CodeAnalysis.LanguageServer";
         args = ["--logLevel" "Error" "--extensionLogDirectory" "/tmp/"];
       };
+      */
 
       language = [
+        {
+          name = "nix";
+          language-servers = ["nixd"];
+          formatter = {
+            command = "alejandra";
+            args = ["--quiet"];
+          };
+        }
+        /*
         {
           name = "c-sharp";
           language-servers = ["roslyn-ls"];
         }
+        */
       ];
     };
-    */
   };
 
   home.packages = with pkgs; [
