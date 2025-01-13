5545283f8bc691e854146271eea8ad3129aee075
Kim Sevandersson <kim@kotee.co>
2025-01-13 10:56:47 +0100

    Update and add tidal

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,9 +1,13 @@
-{
+{pkgs, ...}:{
   imports = [
     ./git.nix
     ./vscode.nix
     ./helix.nix
     ./alacritty.nix
     ./rider.nix
+  ];
+
+  home.packages = with pkgs; [
+    tidal-hifi
   ];
 }
